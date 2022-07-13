46d58365cde15c010358425463315d780dd6acde
rafael <rafael.smascarenhas@gmail.com>
2022-07-12 21:22:00 -0300

    exercicio 17

---
+++ js/custom.js
@@ -0,0 +1,10 @@
+// forma de evitar o conflito com o jQuery e instancia;
+//jQuery( function ($){
+$(document).ready(function(){
+
+    $('.owl-carousel').owlCarousel();
+    $('h4') // tag
+    $('.featured-item') //classe
+    $('#featured') // id
+    $('.featured-item a').addClass('btn btn-success')
+}) 
